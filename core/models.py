7fcad849818d51d66f54a280324d38c623992f7f
Luciano Ratamero <luciano@ratamero.com>
2018-03-24 22:00:25 -0300

    changes product size field to enable updating via schemas.

--- core/models.py
+++ core/models.py
@@ -4,12 +4,12 @@ from django.db import models
 
 class Product(models.Model):
     SIZE_CHOICES = (
-        (1, 'small'),
-        (2, 'medium'),
-        (3, 'large')
+        ('small', 'small'),
+        ('medium', 'medium'),
+        ('large', 'large'),
     )
 
     name = models.CharField(max_length=100)
     in_stock = models.BooleanField()
     rating = models.PositiveSmallIntegerField(null=True, blank=True)
-    size = models.PositiveSmallIntegerField(choices=SIZE_CHOICES, null=True, blank=True)
+    size = models.CharField(max_length=100, choices=SIZE_CHOICES, null=True, blank=True)
