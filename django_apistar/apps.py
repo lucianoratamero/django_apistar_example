59249d0e1a95989dc375225c0ff84a276c6a9972
Luciano Ratamero <luciano@ratamero.com>
2018-03-25 17:58:39 -0300

    tries to fix heroku deploy

---
+++ django_apistar/apps.py
@@ -0,0 +1,8 @@
+
+from django.apps import AppConfig
+
+
+class DjangoAPIStarConfig(AppConfig):
+    """Lean AppConfig for binding API Star to Django"""
+    name = 'django_apistar'
+    verbose_name = "Django API Star"
