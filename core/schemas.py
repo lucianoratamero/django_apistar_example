ebfc5899baea44bd51ea8d3e3a5107f0639ab01c
Luciano Ratamero <luciano@ratamero.com>
2018-07-30 16:04:39 -0300

    uses ProductName schema for Product type.

--- core/schemas.py
+++ core/schemas.py
@@ -7,7 +7,7 @@ class ProductName(validators.String):
 
 class Product(types.Type):
     id = validators.Integer(allow_null=True)
-    name = validators.String(max_length=100)
+    name = ProductName()
     rating = validators.Integer(minimum=1, maximum=5, allow_null=True)
     in_stock = validators.Boolean()
     size = validators.String(enum=['small', 'medium', 'large'], allow_null=True)
